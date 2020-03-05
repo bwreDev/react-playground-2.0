4849514ec35d921ccf1678694aa16c1a51943bde
Colton Bonner <cbonner26118@gmail.com>
2020-03-04 21:29:42 -0700

    First commit on React learning

---
+++ src/Project-tiles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectTiles() {
+    return (
+        <div className='project'>
+            Add your projects.
+        </div>
+    )
+}
+
+export default ProjectTiles;
